df6d82a657e51b2ab383f73d78847cd67282ae33
Chaos Eternal <chaoseternal@gmail.com>
2018-03-22 10:54:35 +0800

    new post done

---
+++ metadata.el
@@ -0,0 +1,43 @@
+(setq org-static-blog-publish-title "Chaos Eternal")
+(setq org-static-blog-publish-url "http://chaoseternal.net/")
+(setq org-static-blog-publish-directory "~/archive/ChaosEternal.github.io/")
+(setq org-static-blog-posts-directory "~/archive/ChaosEternal.github.io/posts/")
+(setq org-static-blog-drafts-directory "~/archive/ChaosEternal.github.io/drafts/")
+(setq org-export-with-toc nil)
+(setq org-export-with-section-numbers nil)
+
+(setq org-static-blog-page-header
+      "<meta  name=\"author\" content=\"Jun Sheng\" />
+<link href=\"http://fonts.googleapis.com/css?family=Roboto&subset=latin\" rel=\"stylesheet\" type=\"text/css\">
+<link href=\"http://fonts.googleapis.com/css?family=Ubuntu+Mono\" rel=\"stylesheet\" type=\"text/css\">
+<link href=\"static/style.css\" rel=\"stylesheet\" type=\"text/css\" />
+<link rel=\"icon\" href=\"static/favicon.ico\">
+<link rel=\"apple-touch-icon-precomposed\" href=\"static/favicon-152.png\">
+<link rel=\"msapplication-TitleImage\" href=\"static/favicon-144.png\">
+<link rel=\"msapplication-TitleColor\" href=\"#0141ff\">
+<script type=\"text/javascript\" src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML\"> </script>
+<meta http-equiv=\"content-type\" content=\"application/xhtml+xml; charset=UTF-8\" />
+<meta name=\"viewport\" content=\"initial-scale=1,width=device-width,minimum-scale=1\">
+")
+
+(setq org-static-blog-page-preamble
+      "<div id=\"preamble\" class=\"status\"><div class=\"header\">
+  <a href=\"http://chaoseternal.net\">Chaos Eternal</a>
+  <div class=\"sitelinks\">
+    <a href=\"https://twitter.com/chaoseternal\">Twitter</a> | <a href=\"http://github.com/Chaoseternal\">Github</a>
+  </div>
+</div>"
+      )
+
+(setq org-static-blog-page-postamble
+"<div id=\"postamble\" class=\"status\"><div id=\"archive\">
+  <a href=\"archive.html\">Other posts</a>
+</div>
+<center><a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/3.0/\">
+<img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-sa/3.0/88x31.png\" /></a>
+<br />
+<span xmlns:dct=\"http://purl.org/dc/terms/\" href=\"http://purl.org/dc/dcmitype/Text\" property=\"dct:title\" rel=\"dct:type\">chaoseternal.net</span> 
+by <a xmlns:cc=\"http://creativecommons.org/ns#\" href=\"http://chaoseternal.net\" property=\"cc:attributionName\" rel=\"cc:attributionURL\">Chaos Eternal</a> 
+is licensed under a <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/3.0/\">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.
+</center>
+")
